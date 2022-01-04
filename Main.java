d8618ce1352a573872ad299aa7b15cac0c7806ff
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2022-01-04 11:56:45 +0100

    Removed unused import statements

--- Main.java
+++ Main.java
@@ -1,11 +1,5 @@
 package banking;
 
-import org.sqlite.SQLiteDataSource;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class Main {
     public static void main(String[] args) {
         String url = "jdbc:sqlite:./" + args[1];
